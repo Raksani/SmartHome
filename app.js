2a231ae3ec0e8bd508fefbc684d6e2973659efcb
Anawin Wongsadit <imonmac@hotmail.com>
2018-07-20 09:46:50 +0700

    Add getHistory API

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 let apiUrl = 'http://ecourse.cpe.ku.ac.th:1515/api/'
 let apiPrefix = 'palmyut-'
-function getVal(name,callback){
+function getVal(name, callback, type) {
     /* 
     name - Key Name without apiPrefix (ex: temperature)
     callback - Function which calls on finish
     */
+    if (!type) type = "view"
     $.ajax({
         type: "GET",
-        url: `${apiUrl}${apiPrefix}${name}/view/`,
+        url: `${apiUrl}${apiPrefix}${name}/${type}/`,
         dataType: "text",
         success: function (response) {
-            if(typeof callback =="function") callback(response)
+            if (typeof callback == "function") callback(response)
         },
         fail: function (response) {
             console.log(`Failed AJAX getting [${name}] value: ${response}`)
-            if(typeof callback =="function") callback(null)
+            if (typeof callback == "function") callback(null)
         }
     });
 }
-function saveVal(name,val,callback){
+function saveVal(name, val, callback) {
     $.ajax({
         type: "POST",
         url: `${apiUrl}${apiPrefix}${name}/set/`,
-        data: {value: val},
+        data: { value: val },
         dataType: "json",
         success: function (response) {
-            if(response.status){
-                if(response.status=='success'){
-                    if(typeof callback =="function") callback(true)
+            if (response.status) {
+                if (response.status == 'success') {
+                    if (typeof callback == "function") callback(true)
                 } else {
                     console.log(`Failed API saving [${name}] value`)
-                    if(typeof callback =="function") callback(false)
+                    if (typeof callback == "function") callback(false)
                 }
             } else {
                 console.log(`API not return status`)
@@ -38,10 +39,11 @@ function saveVal(name,val,callback){
         },
         fail: function (response) {
             console.log(`Failed AJAX saving [${name}] value: ${response}`)
-            if(typeof callback =="function") callback(false)
+            if (typeof callback == "function") callback(false)
         }
     });
 }
-$(function(){
-    
+function getHistory(name, callback) { getVal(name, (typeof callback != "undefined") ? callback : null, "history") }
+$(function () {
+
 })
